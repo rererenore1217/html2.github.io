58963115a145981a516813d910566957436d1bef
rererenore1217 <108104831+rererenore1217@users.noreply.github.com>
2022-09-16 00:59:32 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/登録')
 def review():
-    return render_template('review.html')
+    return render_template("review.html")
 
 @app.route('/output', methods=['POST'])
 def output():
-    data1 = request.form['data1']
-    data2 = request.form['data2']
-    data3 = request.form['data3']
-    data4 = request.form['data4']
-    data5 = request.form['data5']
-    data6 = request.form['data6']
-    data7 = request.form['data7']
-    return  data1+':'+data2+':'+data3+':'+data4+':'+data5+':'+data6+':'+data7
+    data1 =request.form["data1"]
+    data2 =request.form["data2"]
+    data3 =request.form["data3"]
+    data4 =request.form["data4"]
+    data5 =request.form["data5"]
+    data6 =request.form["data6"]
+    data7 =request.form["data7"]
+    return data1+':'+data2+':'+data3+':'+data4+':'+data5+':'+data6+':'+data7
 
 if __name__=="__main__":
     app.run(debug=True)
